8eebf437a1a7e922c319c03bba84896144a94ebe
ishachaudhary01 <126600012+ishachaudhary01@users.noreply.github.com>
2023-03-15 16:50:42 +0000

    Update index.php

--- index.php
+++ index.php
@@ -49,12 +49,6 @@
                 <!--Insert profile link in placeholder-->
                 <li><a class="nav-link" href="#">@username</a></li>
                 </ul>
-                <!--
-                <form class="d-flex" role="search">
-                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                    <button class="btn btn-outline-success" type="submit">Search</button>
-                </form>
-                -->
                 </div>
             </div>
         </nav>
@@ -62,4 +56,3 @@
     
 </body>
 </html>
-
