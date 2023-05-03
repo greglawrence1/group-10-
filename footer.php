b603ff20a2b575b96b475c19dbc6bd9a5c9eacb3
ishachaudhary01 <126600012+ishachaudhary01@users.noreply.github.com>
2023-05-03 12:56:32 +0100

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="footer.css">
+    <title>Footer</title>
+    <style>
+        
+    </style>
+</head>
+<body>
+    <div id="page-container">
+        <div id="content-wrap">
+
+        </div>
+    </div>
+    <div class="footer">
+        <footer style="background-color: #f2e3d0; color:black;">
+        <h2><center>Tenner</center></h2>
+        <!--
+            <hr style="width: 60px; background-color: #b0c7dd; height: 2px; border:none;" />
+        -->
+        </br>
+        <p><center>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ac mauris erat. Ut sed lobortis est. Fusce euismod eget lorem non eleifend. Nulla at urna eget lectus consectetur mattis. Quisque nibh nisl, finibus id finibus vitae, venenatis sit amet sem. Nulla viverra tortor augue, malesuada vulputate nisi pulvinar ut. Maecenas ultricies sodales elit at euismod. Curabitur a turpis mattis, aliquet erat id, pellentesque augue.</center> </p>
+        </br></br>
+        <div><center>
+            <a href="" style="color:black">Your Account</a>&emsp;
+            <a href="" style="color:black">Contact Us</a>&emsp;
+            <a href="" style="color:black">Search</a>&emsp;
+            <a href="" style="color:black">Tutorial</a>
+            </center>
+        </div>
+       
+        </br>
+        <div style="background-color: rgba(0, 0, 0, 0.2); height:3em;">
+            <center></br>© 2020 Copyright</center>
+        </div>
+    </footer>
+    </div>
+   
+</body>
+</html>
