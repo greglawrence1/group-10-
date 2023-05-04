1368aa430a4ba724bdb1f25cb3900f72ad9fde70
jamesm2101 <92738504+jamesm2101@users.noreply.github.com>
2023-05-05 00:04:28 +0100

    Add files via upload

---
+++ aboutpage.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="extradesign.css">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    include 'index.php';
+    ?>
+    <div class="paymentbox">
+        <h3><b>About Tenner</b></h3></br>
+        <p>Tenner was created to connect the Artist to the Customer in a few simple steps. The weight is taken out of the Artist's hands due to the interface between them an the user, creating a seamless transaction between the two. </p>
+    </div>
+    <?php
+    include 'footer.php';
+    ?>
+</body>
+</html>
---
+++ contactus.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="extradesign.css">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    include 'index.php';
+    ?>
+    <div class="paymentbox">
+        <h3><b>Contact Information</b></h3></br>
+        <p>Leeds Beckett Headingley Campus</p>
+        <p>Churchwood Avenue</p>
+        <p>Leeds</p>
+        <p>LS6 3QS</p>
+        <p>07453622173</p>
+        <p>https://github.com/greglawrence1/group-10-</p>
+    </div>
+    <?php
+    include 'footer.php';
+    ?>
+</body>
+</html>
