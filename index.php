1fc8422b743fb12705f2dad80cd76c1a57bdc751
maxbeardy <132579655+maxbeardy@users.noreply.github.com>
2023-05-10 11:06:36 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                   html>
+<html>
+    <head>
+        <title>TBD</title>
+        <!-- <link rel="stylesheet" type="text/css" href="./style.css"> -->
+        <style>
+            .center-text{
+                margin-top: 100px;
+                margin-left: 50px;
+            }
+        </style>
+    </head>
+    <body>
+        <?php session_start(); ?>
+        <header>
+            <div>
+                <?php include_once("navigationbar.php");
+                ?>
+            </div>
+        </header>
+        <div class="center-text">
+            <h2>Welcome: User </h2><br>
+            <h2>Total Orders: </h2><br>
+            <h2>Total Revenue: </h2><br>
+            <h2>Average Rating </h2>
+            
+
+        <footer>
+            <div>
+                <?php include_once("footer.php"); ?>
+            </div>
+        </footer>
+</body>
+</html>
+
